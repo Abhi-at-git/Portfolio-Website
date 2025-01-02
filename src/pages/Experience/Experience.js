59dc26195935e90c8bf0b8450b06ed5621c3f419
Abhinav <akki12042003@gmail.com>
2025-01-02 09:45:45 +0530

    Experience page updated

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
+import ProjectCard from "../../components/ExperienceCard/ExperienceCard";
+import lysto from "../../assets/images/lysto.jpeg";
+import Navbar from "../../components/Navbar/Navbar";
+import './style.css'
 
 
 const Experience = () => {
+  const projectData = [
+    {
+      title: "Lysto",
+      description: [
+        "Game Play Tester",
+        "Tested the game for bugs and glitches.",
+        "Suggested improvements in the game.",
+      ],
+      image: lysto,
+      websiteLink: "https://lysto.gg/",
+    }
+  ];
+
   return (
-    <div>
-        <h1>Experience</h1>
+    
+    <div className="projects-container">
+      <Navbar />
+      <h1>Experiences</h1>
+      {projectData.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          description={project.description}
+          image={project.image}
+          websiteLink={project.websiteLink}
+        />
+      ))}
     </div>
   );
 };
